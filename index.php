783662f5f7b5c37a957894b21f9cecea9ad11a47
Bagorogile-Mphaka <103036064+Bagorogile-Mphaka@users.noreply.github.com>
2023-04-08 13:11:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <title>Football application</title>
+    <h1>
+        <?php echo "Football App"; ?>
+    </h1>
+    <body>  
+
+    <ul>
+            <li><b><a href="">Home</a></b></li>
+            <li ><b><a href="competitions.php">Competitions</a></b></li>
+            <li><b><a href="teams.php">Teams</a></b></li>
+            <li><b><a href="fixtures.php">Fixtures</a></b></li>
+            <li><b><a href="player_positions.php">Player positions</a></b></li>
+            <li><b><a href="players.php">Players</a></b></li>
+            <li><b><a href="player_fixtures.php">Player fixtures</a></b></li>
+            <li><b><a href="reports.php">Reports</a></b></li>
+        </ul>
+    </body>
+        
+    
+</html>
---
+++ player_positions.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <title>Player positions</title>
+    <h1><?php echo "Player positions"; ?></h1>
+    <body>
+
+        <table>
+            <tr>
+                <th>Player positions</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>Goal keeper</td>
+                <td></td>
+            </tr>
+            <tr>
+            <td>Central defender</td>
+            <td></td>
+            </tr>
+            <tr>
+                <td>Central defensive midfielder</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Left back</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Right back</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Left winger</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Right winger</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Left forward</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Right forward</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Striker</td>
+                <td></td>
+            </tr>
+
+        </table>
+    </body>
+</html>
